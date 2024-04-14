438365130ce7ee95fbac68a99841039d88a2c6c1
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-04-14 08:15:45 -0300

    teste

---
+++ back-and/variaveis.js
@@ -0,0 +1,19 @@
+// MANEIRAS DE ESCRITA PARA UTILIZAR VARIAVEIS
+
+// var qualquercoisa = 'felipe'
+// var QualquerCoisa = 'felipe'
+// var qualquerCoisa = 'felipe'
+// var Qualquer_coisa = 'felipe'
+
+//var nomep = 'Felipe';
+
+// console.log(nome)
+
+let nome = 'Fábio Lima'
+let idade = 18
+
+console.log(nome, idade)
+
+const email = 'fabio.felipe@gmail.com'
+
+console.log(email)
