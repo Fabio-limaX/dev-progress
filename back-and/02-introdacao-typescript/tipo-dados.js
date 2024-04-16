104e1a43a562cd1ae35867d3e4a49705e3323e24
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-04-16 18:52:12 -0300

    Introdução de TypeScript

---
+++ back-and/02-introdacao-typescript/tipo-dados.js
@@ -0,0 +1,22 @@
+let nome = 'Fábio Lima' // string
+console.log(typeof nome)
+
+let idade  = 18
+console.log(typeof idade) //number
+
+let altura = 1.84 // number
+console.log(typeof altura)
+
+let ativo = true  // Bloolean
+ativo = false // Bloolean
+console.log(typeof ativo)
+
+let cor = 'azul' // String
+console.log(typeof cor)
+
+cor = null // Tipo null ou Object
+console.log(typeof cor)
+
+let sobrenome // undefined
+sobrenome = 'Lima'
+console.log(typeof sobrenome)
---
+++ back-and/02-introdacao-typescript/tipos-dados-2.ts
@@ -0,0 +1,8 @@
+let nome = 'Fábio lima'
+console.log(nome)
+console.log(typeof nome)
+
+let altura: number = 18
+
+let idade: any = 33
+idade = 'fabio' 
