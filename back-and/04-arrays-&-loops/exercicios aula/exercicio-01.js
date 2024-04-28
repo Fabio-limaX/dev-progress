fa7fbec8709c45c109d2c35b392686796ff9881d
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-04-28 19:19:59 -0300

    Aulas de arrays PT-1

---
+++ back-and/04-arrays-&-loops/exercicios aula/exercicio-01.js
@@ -0,0 +1,23 @@
+// 1. Crie um array com 5 nomes de países 
+// 2. adicione um país ao fim da lista
+// 3. Imprima a lista na tela 
+// 4. Altere o quinto país da lista para um novo país 
+// 5. Imprima a lista na tela
+// 6. Imprima o último país da lista na tela 
+// 7. imprima o segundo país na lista na tela 
+// 8. Imprima o país de índece 2 na tela
+
+
+const nomesPaises = ['Brasil', 'Argentina', 'França', 'USA', 'Russia']
+
+nomesPaises[nomesPaises.length] = 'Alemanha'
+
+console.log(nomesPaises)
+
+nomesPaises[4] = 'Polonia'
+
+console.log(nomesPaises)
+
+console.log(nomesPaises[5])
+console.log(nomesPaises[1])
+console.log(nomesPaises[2])
