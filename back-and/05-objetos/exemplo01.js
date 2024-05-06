30891e64b99902c533122cc87d604dd9b0b62a57
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-05-06 17:00:25 -0300

    Objetos

---
+++ back-and/05-objetos/exemplo01.js
@@ -0,0 +1,14 @@
+const nome = 'Felipe'
+const idade = 33
+const temCNH = true
+
+const usuario = { 
+    nome: 'Fábio',
+    Idade: 18, 
+    temCNH: true, 
+    habilidades: ['JS', 'PHP', 'Java', 'HTML']
+}
+
+console.log(['nome'][2])
+
+console.log(usuario)
