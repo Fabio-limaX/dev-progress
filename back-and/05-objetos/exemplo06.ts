6df97f5808f0de17fb873bab4759ee5ee6c67312
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-05-07 23:27:14 -0300

    Atualização dos erros nas pastas

--- back-and/05-objetos/exemplo06.ts
+++ back-and/05-objetos/exemplo06.ts
@@ -1,3 +1,12 @@
+//Desestruturação de um objeto
+//Sintaxe para desestruturação: const { propriedade1, propriedade2 } = objeto
+//Extraindo informações de um objeto e armazenando em variáveis
+//Exemplo prático de desestruturação com objeto contendo usuários
+//Acessando propriedades de um objeto desestruturado
+//Utilização de desestruturação com objeto dentro de objeto
+//Exemplo prático com objeto de endereço
+//Acessando propriedades de um objeto interno desestruturado
+
 const usuario = {
     nome: 'Fabio',
     email: 'Fabio@gmail.com',
--- back-and/05-objetos/exmplo07.ts
+++ back-and/05-objetos/exmplo07.ts
@@ -1,3 +1,7 @@
+// Aula ensina a Desestruturação de arrays
+
 const pessoas = ['Fabio', 'Pedro', 'Maria']
 
-const []
+const [a , b, c] = pessoas //Podendo colocar qualquer nome!
+
+console.log(a, b, c)
