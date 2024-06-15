d36530f7582362b9d59784ff6bd47a3dd7a47749
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-06-15 13:12:17 -0300

    Aula de Array I

---
+++ back-and/10-metodos-de-arrays/exercicio-aula/ex01.ts
@@ -0,0 +1,13 @@
+const arrayPaises = ['Brasil', 'Argentina', 'Alemanha', 'Portugal']
+
+arrayPaises.push('Equador')
+
+console.log(arrayPaises);
+
+arrayPaises.pop()
+
+console.log(arrayPaises);
+
+arrayPaises.unshift('Polonia')
+
+console.log(arrayPaises);
