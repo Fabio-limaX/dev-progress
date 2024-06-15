3d6b92861b659f1972f90fd53372b8b010dc7a7a
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-06-15 17:03:35 -0300

    .

--- back-and/Revisao M01/src/index.ts
+++
@@ -1,6 +0,0 @@
-// Se a pessoa estiver entre os aprovados, iremos
-// retornar a colocação dela no concurso;
-// se não, retornamos a mensagem "NAO APROVADO"
-
-const nomeDigitado = "Ariel";
-const listaAprovadosOrdenada = ["Camila", "Lucas", "Ariel", "Fernado", "Ana"];
