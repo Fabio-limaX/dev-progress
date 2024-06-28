da345d800596360b27625b41c61ebc26aac7697a
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-06-28 17:30:25 -0300

    configuração do build da aplicação

--- back-and/13-testes-automatizados/subtracao.test.ts
+++ back-and/13-testes-automatizados/subtracao.test.ts
@@ -2,12 +2,12 @@ import subtracao from "./subtracao";
 
 describe('testes de subtração', () => {
     test('deve verificar o resultado de uma subtração', () => {
-        expect(subtracao(6, 2)).toBe(4)
-        expect(subtracao(6, 8)).toBe(-2)
-        expect(subtracao(60, 2)).toBe(58)
+        expect(subtracao(6.7, 2.5)).toBeCloseTo(4.2)
+        expect(subtracao(6, 8)).toBeCloseTo(-2)
+        expect(subtracao(60, 2)).toBeCloseTo(58)
     })
 
     test('deve verificar o resultado incorreto de uma subtração', () => {
-        expect(subtracao(6, 2)).toBe(5)
+        expect(subtracao(6, 2)).toBeCloseTo(5)
     })
 })
