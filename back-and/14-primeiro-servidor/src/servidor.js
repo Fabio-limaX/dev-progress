a38660e51ee3e848c52d1153d2f7ce2f45369958
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-06-28 21:31:41 -0300

    Criando um servidor com festify

---
+++ back-and/14-primeiro-servidor/src/servidor.js
@@ -0,0 +1,15 @@
+import fastify from 'fastify';
+
+//const servidor = fastify({})
+const servidor = fastify({
+    logger: true
+})
+
+servidor.get('/teste', function controlador(requisicao, resposta) {
+    const saudacao = 'Bem vindo ao meu primeiro servidor Node.js'
+    return resposta.send(saudacao)
+})
+
+servidor.listen({
+    port: 3000
+})
