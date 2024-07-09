cd2109f99718383add93ee3982a75574bb5c86f7
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-07-09 00:21:39 -0300

    intermediarios

---
+++ back-and/16-ROTAS-INTERMEDIARIOS-CONTROLADORES/src/controladores.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from 'express'
+
+const pessoas = [
+    {nome: 'guido', email: 'fabio@email.com'},
+    {nome: 'joao', email: 'joao@email.com'},
+    {nome: 'kevin', email: 'kevin@email.com'},
+]
+
+export const itemProdutos =  (req: Request, res: Response) => {
+    console.log(req.params.item)
+    return res.send('Server is configured with typescript')
+}
+
+
+export const buscarUsuario = (req: Request, res: Response) => {
+    const{ email } = req.params
+
+    const pessoa = pessoas.find(item => {
+        return item.email === email
+    })
+
+    if(!pessoa) {
+        return res.send('Pessoa não encontarada!')
+    }
+
+    return res.send(pessoa)
+}
+
+export const buscarUsuarioQuery = (req: Request, res: Response) => {
+    const { email } = req.query
+
+    if (!email) {
+        return res.send('Parametro não informado')
+    }
+
+    const pessoa = pessoas.find((item) => {
+        return item.email === email
+    })
+
+    if(!pessoa) {
+        return res.send('Pessoa não encontrada!')
+    } 
+
+    return res.send(pessoa) 
+}
--- back-and/16-ROTAS-INTERMEDIARIOS-CONTROLADORES/src/index.ts
+++ back-and/16-ROTAS-INTERMEDIARIOS-CONTROLADORES/src/index.ts
@@ -1,52 +1,16 @@
-import "dotenv/config";
-
+import 'dotenv/config'
 import express from 'express'
+import { buscarUsuario, buscarUsuarioQuery, itemProdutos } from './controladores'
+import { intermediarioGeral, meuPrimeiroIntermediario } from './intermediarios'
 
 const servidor = express()
 
-servidor.get('/produtos/:item', (req, res) => {
-    console.log(req.params.item)
-    return res.send('Servidor o nodemon está configurado com o typescript!')
-})
+servidor.use(intermediarioGeral)
 
-const pessoas = [
-    {nome: 'fabio', email: 'fabio@gmail.com'},
-    {nome: 'maria', email: 'maria@gmail.com'},
-    {nome: 'pedro', email: 'pedro@gmail.com'}
-]
+servidor.get('/produtos/:item', meuPrimeiroIntermediario, itemProdutos)
 
-servidor.get('/usuarios/:email', (req, res) => {
-    const {email} = req.params
+servidor.get('/usuarios/:email', buscarUsuario)
 
-    const pessoa = pessoas.find((item) => {
-        return item.email === email
-    })
-
-    if (!pessoa) {
-        return res.send('pessoa não encontrada')
-    }
-
-    return res.send(pessoa)
-})
-
-
-servidor.get('/usuarios', (req, res) => {
-   // console.log(req.query);
-    const { email } = req.query
-
-    if (!email) {
-        return res.send('paramentro não encontrado')
-    }
-
-    const pessoa = pessoas.find((item) => {
-        return item.email === email
-   })
-
-    if (!pessoa) {
-        return res.send('pessoa não encontrada')
-    }
-
-    return res.send('Ok')
-})
+servidor.get('/usuarios', buscarUsuarioQuery)
 
 servidor.listen(process.env.PORT)
