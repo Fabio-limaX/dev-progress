8164663dc273d2cc96ef653a3182a910c6313abe
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-07-10 11:01:17 -0300

    Configuração do Middleware de json do express

--- back-and/17-API-REST/src/index.ts
+++ back-and/17-API-REST/src/index.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config'
-import Express  from "express";
+import express  from "express";
 import rotas from './rotas';
 
-const app = Express()
+const app = express()
+
+app.use(express.json())
 
 app.use(rotas)
 
