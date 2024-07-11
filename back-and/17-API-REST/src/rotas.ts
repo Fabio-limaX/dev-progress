89b73af54a4b45bc3feb1e3179953bf597fd58d2
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-07-10 21:51:12 -0300

    f

--- back-and/17-API-REST/src/rotas.ts
+++ back-and/17-API-REST/src/rotas.ts
@@ -5,4 +5,13 @@ const rotas = Router()
 
 rotas.get('/', teste)
 
+// Listar todos os intrutores
+// detalhar as informações de um instrutor
+// cadastrar instrutor 
+// editar um istrutor 
+// excluir um instrutor
+
+// cadastrar uma aula para um instrutor 
+// excluir uma aula para um instrutorn
+
 export default rotas
