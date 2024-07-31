f4978945a994de8277ceb240cd3d43cbb2bcc11f
Fabio-limaX <fabiolimadesenvolvedor@gmail.com>
2024-07-31 20:28:57 -0300

    Prática de leitura e escrita em arquivos com promises

---
+++ back-and/Modulo_03/Aulas/07-ASSICRONISMO-E-LEITURA-E-ESCRITA-EM-ARQUIVOS/05-EXEMPLO.ts
@@ -0,0 +1,22 @@
+import fs from 'fs/promises'
+
+fs.readFile('./bancodedados.json').then((resultado) => {
+	const json = resultado.toString()
+	const bancodedados = JSON.parse(json)
+
+	bancodedados.usuarios.push({
+		nome: 'João',
+		email: 'joao@email.com'
+	})
+
+	bancodedados.usuarios.push({
+		nome: 'Maria',
+		email: 'maria@email.com'
+	})
+
+	fs.writeFile('./bancodedados.json', JSON.stringify(bancodedados))
+
+	console.log(bancodedados)
+}).catch(() => {
+	console.log('Arquivo não existe!')
+})
